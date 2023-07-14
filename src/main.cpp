ce703ef9e7e1e418cfc7f6a862b85db469c22a56
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-15 06:13:03 +0700

    update telegram bot send message

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
 #include <Arduino.h>
 
 #define RST_PIN D1
@@ -12,10 +16,13 @@ int myFunction(int, int);
 const int sensorPin = A0;
 const char* ssid = "Kali-Linux";
 const char* password = "kaliroot";
-const char* botToken = "ewiurweiiweurhwieuoiewjroi";
-const int telegramChatID = "21312";
+#define BOTtoken "XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#define CHAT_ID "XXXXXXXXXX"
 
 MFRC522 rfid(SS_PIN, RST_PIN);
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,6 +30,28 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
+
+  configTime(0, 0, "pool.ntp.org");
+  client.setTrustAnchors(&cert);
+
+  // connecting to wifi config
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
   
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -60,16 +89,19 @@ void loop() {
     digitalWrite(RELAY_PIN, HIGH);
     digitalWrite(BUZZER_PIN, LOW);
     // send notification to telegram berhasil membuka pintu
+    bot.sendMessage(CHAT_ID, "Pintu terbuka!!", "");
   } else {
     delay(2000);
     int sensorValue = analogRead(sensorPin);
     if(sensorValue > 0){
       digitalWrite(BUZZER_PIN, HIGH);
       // send notification to telegram ada yang masuk
+      bot.sendMessage(CHAT_ID, "Pintu terbuka! dan ada yang mencoba masuk!", "");
     }
     digitalWrite(RELAY_PIN, HIGH);
     digitalWrite(BUZZER_PIN, HIGH);
     // send notification to telegram gagal membuka pintu
+    bot.sendMessage(CHAT_ID, "Door Opened without authorization!", "");
   }
 }
 
