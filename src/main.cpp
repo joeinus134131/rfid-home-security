7223ac72cf943603b57f2270e6a447023c08858f
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-15 05:53:30 +0700

    perbaikan logic pembukaan pintu

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,60 @@
 #include <SPI.h>
-#include <RFID.h>
+#include <MFRC522.h>
 #include <Arduino.h>
 
-#define SS_PIN D4 
-#define RST_PIN D3
+constexpr uint8_t RST_PIN = 5;
+constexpr uint8_t SS_PIN = 4;
+#define BUZZER_PIN D1
+#define RELAY_PIN D2
 
 // put function declarations here:
 int myFunction(int, int);
-const int relayPin = 8;
-const int buzzerPin = 9;
 const int sensorPin = A0;
 const char* ssid = "Kali-Linux";
 const char* password = "kaliroot";
-const char* botToken = "";
-const int telegramChatID = ;
+const char* botToken = "ewiurweiiweurhwieuoiewjroi";
+const int telegramChatID = "21312";
 
-RFID rfid(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+MFRC522::MIFARE_Key key;
+
+byte nuidPICC[4];
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   Serial.begin(9600);
-  rfid.init();
-  pinMode(relayPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
+  SPI.begin();
+  rfid.PCD_Init();
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   pinMode(sensorPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int rfid = true;
-  if(rfid){
-    digitalWrite(relayPin, LOW);
-    delay(1000);
+  if(!rfid){
     int sensorValue = analogRead(sensorPin);
-    Serial.print(sensorValue);
     if(sensorValue > 0){
-      delay(5000);
-      digitalWrite(buzzerPin, HIGH);
-      // send notification to telegram
-    } else {
-      digitalWrite(relayPin, HIGH);
+      digitalWrite(BUZZER_PIN, HIGH);
+      // send notification to telegram ada yang masuk
     }
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, HIGH);
+    // send notification to telegram gagal membuka pintu
+  } else {
+    digitalWrite(RELAY_PIN, LOW);
+    delay(5000); // delay untuk akses buka pintu
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, LOW);
+    // send notification to telegram berhasil
   }
 }
 
