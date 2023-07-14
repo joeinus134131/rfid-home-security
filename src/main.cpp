cda36b4c484a61eee14cbc1778bebdb7a7964b43
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-15 06:03:12 +0700

    menambahkan module rfid logic

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <MFRC522.h>
 #include <Arduino.h>
 
-constexpr uint8_t RST_PIN = 5;
-constexpr uint8_t SS_PIN = 4;
-#define BUZZER_PIN D1
-#define RELAY_PIN D2
+#define RST_PIN D1
+#define SDA_PIN D2
+#define BUZZER_PIN D3
+#define RELAY_PIN D4
 
 // put function declarations here:
 int myFunction(int, int);
@@ -17,20 +17,12 @@ const int telegramChatID = "21312";
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-MFRC522::MIFARE_Key key;
-
-byte nuidPICC[4];
-
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   Serial.begin(9600);
   SPI.begin();
-  rfid.PCD_Init();
-
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
+  mfrc522.PCD_Init();
   
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -39,8 +31,37 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int rfid = true;
-  if(!rfid){
+  if(!mfrc522.PICC_IsNewCardPresent()){
+    return;
+  }
+
+  if(!mfrc522.PICC_ReadCardSerial()){
+    return;
+  }
+
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+
+  for(byte i = 0; i < mfrc522.uid.size; i++){
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  Serial.println();
+  Serial.print("Pesan : ");
+  content.toUpperCase();
+
+  if(content.substring(1) == "D9 DF 60 B9"){
+    digitalWrite(RELAY_PIN, LOW);
+    delay(5000); // delay untuk akses buka pintu
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, LOW);
+    // send notification to telegram berhasil membuka pintu
+  } else {
+    delay(2000);
     int sensorValue = analogRead(sensorPin);
     if(sensorValue > 0){
       digitalWrite(BUZZER_PIN, HIGH);
@@ -49,12 +70,6 @@ void loop() {
     digitalWrite(RELAY_PIN, HIGH);
     digitalWrite(BUZZER_PIN, HIGH);
     // send notification to telegram gagal membuka pintu
-  } else {
-    digitalWrite(RELAY_PIN, LOW);
-    delay(5000); // delay untuk akses buka pintu
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(BUZZER_PIN, LOW);
-    // send notification to telegram berhasil
   }
 }
 
